789406d604bcf0e8d894f4773bf3f987519b5774
Julian Bruyers <julian.bruyers@gmail.com>
2026-01-06 19:15:41 +0100

    feat: Implementing Go interface for macOS Touch ID/Face ID call

---
+++ auth_darwin.go
@@ -0,0 +1,35 @@
+//go:build darwin
+
+package touchid
+
+/*
+#cgo CFLAGS: -x objective-c
+#cgo LDFLAGS: -framework LocalAuthentication -framework Foundation
+#include <stdlib.h>
+#include "native/touchid.c"
+*/
+import "C"
+
+import (
+	"unsafe"
+)
+
+func Authenticate(promptMsg string) (bool, error) {
+	cPrompt := C.CString(promptMsg)
+	defer C.free(unsafe.Pointer(cPrompt))
+
+	result := C.AuthenticateUser(cPrompt)
+
+	switch result {
+	case 1:
+		return true, nil
+	case 0:
+		return false, ErrUserCanceled
+	case -1:
+		return false, ErrNotAvailable
+	case -2:
+		return false, ErrInternal
+	default:
+		return false, ErrInternal
+	}
+}
